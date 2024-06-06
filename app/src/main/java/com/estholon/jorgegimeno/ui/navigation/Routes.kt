15f864b93dc3344ed28ce757f5da10aea1a43829
Jorge Gimeno <57326891+jorgila@users.noreply.github.com>
2024-06-06 09:41:51 +0200

    Delete app directory

--- app/src/main/java/com/estholon/jorgegimeno/ui/navigation/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.estholon.jorgegimeno.ui.navigation
-
-sealed class Routes (val route: String) {
-    object SplashScreen : Routes("splash")
-    object SignInScreen : Routes("signIn")
-    object SignUpScreen : Routes("signUp")
-    object RecoverScreen : Routes("recover")
-    object MainScreen : Routes("main")
-}
